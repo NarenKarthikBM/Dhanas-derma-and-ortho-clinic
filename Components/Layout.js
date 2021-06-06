a44661ce5b959d95ddd0445aa4f2e0cbef424491
Babu <babu@Babus-iMac.local>
2021-06-06 17:13:59 +0530

    favicon

--- Components/Layout.js
+++ Components/Layout.js
@@ -19,7 +19,10 @@ export default function Layout({ children , footerstatic, description, title}) {
             <Head>
                 <title>{title}</title>
                 <meta charSet="UTF-8" />
-                <link rel="icon" href="favicon.ico" type="image/x-icon" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
                 <meta name="description" content={description}></meta>
                 <meta name="author" content="Naren Karthik B M" />
                 <meta name="format-detection" content="telephone=yes"></meta>
